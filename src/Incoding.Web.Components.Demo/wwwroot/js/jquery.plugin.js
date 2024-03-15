60db58ba50b69c18065dfb55653f43dcb9f6d9a0
semyon <semyon@planifi.net>
2024-03-15 17:55:53 +0300

    Add excelField input (#42)

--- src/Incoding.Web.Components.Demo/wwwroot/js/jquery.plugin.js
+++ src/Incoding.Web.Components.Demo/wwwroot/js/jquery.plugin.js
@@ -330,23 +330,151 @@
 
     const selectedClass = "selected"
 
+    const current = {
+        $active: null,
+        $input: null,
+        $selection: $()
+    }
+
+    function activateInput() {
+
+    }
+
+    function deactivateInput() {
+
+    }
+
+    function resetSelection() {
+        current.$selection = $(current.$active)
+    }
+
+    function addSelection($current) {
+        current.$selection = current.$selection.add($current)
+    }
+
+    function addRangeSelection($current) {
+        const {
+            $active,
+            $input,
+            $selection
+        } = current
+
+        const cells = [$current[0].cellIndex, $active[0].cellIndex].sort((a, b) => a - b)
+        const rows = [$current[0].parentElement.rowIndex, $active[0].parentElement.rowIndex].sort((a, b) => a - b)
+
+        const $trs = $active.closest('table').find('tr')
+
+        for (let rowIndex = rows[0]; rowIndex < rows[1]; rowIndex++) {
+            const allCells = $trs.eq(rowIndex).find('[excel-field]')
+
+            for (let columnIndex = cells[0]; columnIndex < cells[1]; columnIndex++) {
+                const cell = allCells.eq(columnIndex)
+
+                cell.addClass('selected')
+            }
+        }
+    }
+
+    function applyChange() {
+        const {
+            $active,
+            $input,
+            $selection
+        } = current
+
+        console.log($selection, $active, $input.val())
+        $selection.each(function() {
+            $selection.find('input').val($input.val())
+        })
+    }
+
     $.fn.excelField = function() {
+
+
         return $(this).find('[excel-field]').each(function() {
-            const $cell = $(this)
-            const $input = $cell.find('input')
+            const $cell = $(this).addClass('excel-field').prop('tabindex', 0)
+            const $input = $cell.find('input').prop('tabindex', -1)
 
-            // prevent focus on click
-            $input.addClass('disabled')
+            $cell.on('click', function(event) {
+                if (event.shiftKey) {
+                    return addRangeSelection($cell)
+                }
 
-            $cell.on('click', function() {
-                $cell.addClass('selected')
-                $input.addClass('disabled')
+                current.$active = $cell
+                current.$input = $input
+
+                if (event.ctrlKey) {
+                    console.log('ctrl');
+                    addSelection($cell)
+                }
+                else {
+                    resetSelection()
+                }
+
+                console.log(current.$selection);
+            })
+
+            $cell.on('focus', function() {
+                inputActive = false
+            })
+
+            $cell.on('change', function() {
+                applyChange()
+            })
+
+            $cell.on('focusout', function() {
+
+            })
+
+            $cell.on('keydown', function(event) {
+                if (inputActive)
+                    return
+
+                if (event.which === keyCodes.TAB) {
+
+                } else if (event.which === keyCodes.ESCAPE) {
+
+                } else if (event.key.length === 1) {
+                    $input.val('').focus()
+                    activateInput()
+                }
             })
 
             $cell.on('dblclick', function() {
-                $input.removeClass('disabled')[0].focus()
+                activateInput()
             })
+
+            function activateInput() {
+                $input.removeClass('disabled')[0].focus()
+                inputActive = true
+            }
         })
     }
 
 } (jQuery))
+
+
+const keyCodes = {
+    BACKSPACE: 8,
+    COMMA: 188,
+    DELETE: 46,
+    DOWN: 40,
+    END: 35,
+    ENTER: 13,
+    ESCAPE: 27,
+    HOME: 36,
+    LEFT: 37,
+    NUMPAD_ADD: 107,
+    NUMPAD_DECIMAL: 110,
+    NUMPAD_DIVIDE: 111,
+    NUMPAD_ENTER: 108,
+    NUMPAD_MULTIPLY: 106,
+    NUMPAD_SUBTRACT: 109,
+    PAGE_DOWN: 34,
+    PAGE_UP: 33,
+    PERIOD: 190,
+    RIGHT: 39,
+    SPACE: 32,
+    TAB: 9,
+    UP: 38
+}

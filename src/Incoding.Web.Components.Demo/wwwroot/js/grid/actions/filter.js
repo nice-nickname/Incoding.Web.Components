c9824a37bde6def498a601cbaf94e631f1b8dd80
semyon <semyon@planifi.net>
2024-04-24 18:17:02 +0300

    Improve filter (#4)

--- src/Incoding.Web.Components.Demo/wwwroot/js/grid/actions/filter.js
+++ src/Incoding.Web.Components.Demo/wwwroot/js/grid/actions/filter.js
@@ -74,8 +74,37 @@ class FilterController {
         }
     }
 
-    reset() {
+    reset(column) {
+        const resetIndex = this.filters.findIndex(s => s.column === column)
+        if (resetIndex < 0) {
+            return
+        }
 
+        this.filters.splice(resetIndex, 1)
+
+        if (this.filters.length !== 0) {
+            return this.apply(this.filters[0].column, this.filters[0].criteria)
+        }
+
+        if (this.table.splitGrid) {
+            this.table.splitGrid.data = this.table.splitGrid.originData
+        }
+
+        this.table.parent.siblings.forEach(table => {
+            table.data = table.originData
+        })
+
+        this.table.parent.siblings.forEach(table => {
+            table.removeAllRows()
+
+            if (!table.splitGrid) {
+                table.renderRows()
+            }
+        })
+
+        if (this.table.splitGrid) {
+            this.table.splitGrid.rerender()
+        }
     }
 
     #filterData(data, structure, filterColumn, nextFilters) {
@@ -86,7 +115,7 @@ class FilterController {
             let isVisible = filterColumn.criteria.has(String(value))
 
             if (isVisible && nextFilters.length !== 0) {
-               isVisible = !nextFilters.some(s => s.criteria.has(String(s.getter(item))))
+               isVisible = nextFilters.some(s => s.criteria.has(String(s.getter(item))))
             }
 
             if (isVisible && this.table.isSimpleMode() && structure.nested) {
@@ -105,12 +134,17 @@ class FilterController {
 
         const $menu = $filter.find('[role=filter-menu]')
         const $apply = $filter.find('[role=filter-apply]')
+        const $clear = $filter.find('[role=filter-clear]')
 
         $apply.on('click', () => {
             const criteria = FilterController.collectCriteria($menu)
 
             this.apply(column, criteria)
             this.close()
+        })
+
+        $clear.on('click', () => {
+            this.reset(column)
         })
 
         $filter
@@ -204,6 +238,12 @@ class FilterController {
     <button id="regular-dropdown" class="hidden" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
 
     <ul class="dropdown-menu" role='filter-menu'>
+        <li class="dropdown-item">
+            <a href="javascript:void(0)">
+                <button role='filter-clear'>Clear</button>
+            </a>
+        </li>
+
         <li class="dropdown-item">
             <a href="javascript:void(0)">
                 <button role='filter-apply'>Apply</button>
